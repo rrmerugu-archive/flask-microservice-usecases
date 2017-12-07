f7881df4a801d3ce2eaf37aa3f25e16ddb6cf76f
Ravi Merugu <rrmerugu@gmail.com>
2017-12-07 21:51:11 +0530

    requirements moved to seperate folder with 3 env requirements seperated

---
+++ simple-hello-world/tests/test_app.py
@@ -0,0 +1,28 @@
+from app import app
+import unittest
+
+
+class HelloWordAppTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        self.app = app.test_client()  # creates a test client
+        self.app.testing = True  # propagate the exceptions to the test client
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
